29625160bd1f7da5f825be8146a8fd2902653dbf
Alex Sinfarosa <as898@cornell.edu>
2018-02-09 16:24:15 -0500

    rewriting the functions to get/update query string

--- src/stores/blockStore.js
+++ src/stores/blockStore.js
@@ -1,4 +1,4 @@
-import { observable, action, when } from "mobx";
+import { observable, action } from "mobx";
 
 // model
 import BlockModel from "./blockModel";
@@ -11,22 +11,21 @@ export default class BlockStore {
   app;
   constructor(app) {
     this.app = app;
-    when(() => this.blocks.length === 0, () => this.getQString(this.qString));
+    this.getQString(this.qString);
   }
 
   @observable blocks = [];
 
   // initial state query string
-  @observable qString = this.app.history.location.search;
-  @observable qStringDefault = "?c=Temp/state/maxt/ANN/NY/";
+  @observable
+  qString = this.app.history.location.search || "?c=Temp/state/maxt/ANN/NY/";
 
   @action
   getQString = qString => {
-    console.log(qString);
     if (!qString) {
       console.log("NO string");
-      this.app.history.replace(this.qStringDefault);
-      qString = this.qStringDefault;
+      this.app.history.replace(this.qString);
+      qString = this.qString;
     }
 
     if (qString.includes("&")) {
@@ -42,14 +41,12 @@ export default class BlockStore {
   @action
   setBlock = (qString, i) => {
     let qParam = parseURL(qString);
-    // console.log(qParam);
     const nParam = correctParam(qParam);
-    // console.log(nParam);
     const isValid = isEquivalent(qParam, nParam);
 
     if (!isValid) {
-      this.app.history.replace(this.qStringDefault);
-      qParam = parseURL(this.qStringDefault);
+      this.app.history.replace(this.qString);
+      qParam = parseURL(this.qString);
     }
 
     const bbox = qParam.bbox;

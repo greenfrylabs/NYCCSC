c7e53da8b85f9e0056068316c734fb585bec5995
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-05-02 18:34:05 -0400

    updates deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 if [[ -n $(git status --porcelain) ]]; then echo "repo is not checked in fully. Try again."; exit 1; fi
-rm -rf dist/*
+rm -rf build/*
 npm run build
 
-aws s3 sync --profile=nescaum dist/ s3://ny-datagrapher-nescaum-ccsc-dataservices/ --acl public-read
+aws s3 sync --profile=nescaum build/ s3://ny-datagrapher-nescaum-ccsc-dataservices/ --acl public-read
 
 aws cloudfront create-invalidation --profile=nescaum --distribution-id E2Q0FE395YAO7D --paths "/*"
-rm -rf dist/*
+rm -rf build/*

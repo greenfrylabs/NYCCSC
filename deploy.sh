4a9bcc6b653cb81f155ff3a4eb70683a7290625c
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-05-02 17:59:53 -0400

    updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 if [[ -n $(git status --porcelain) ]]; then echo "repo is not checked in fully. Try again."; exit 1; fi
-echo "We can't build this yet, so using hard-coded... but soon I hope"
-#rm -rf dist/*
-#grunt
+rm -rf dist/*
+npm run build
 
 aws s3 sync --profile=nescaum dist/ s3://ny-datagrapher-nescaum-ccsc-dataservices/ --acl public-read
 
-aws cloudfront create-invalidation --profile=nescaum --distribution-id E2Q0FE395YAO7D --paths "*"
-# rm -rf dist/*
+aws cloudfront create-invalidation --profile=nescaum --distribution-id E2Q0FE395YAO7D --paths "/*"
+rm -rf dist/*
